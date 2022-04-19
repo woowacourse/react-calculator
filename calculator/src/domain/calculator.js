124d570ee788de8896b8d1cba69e01726560a552
JUDONGHYEOK <judonghyeok@gmail.com>
2022-04-19 16:11:31 +0900

    chore: eslint, prettier 설정 및 불필요한 주석제거
    Co-authored-by: jhy979 <jhy979@gmail.com>

--- calculator/src/domain/calculator.js
+++ calculator/src/domain/calculator.js
@@ -1,14 +1,14 @@
-const calculator = {
-  "+": function (a, b) {
+export const calculator = {
+  '+': function (a, b) {
     return a + b;
   },
-  "-": function (a, b) {
+  '-': function (a, b) {
     return a - b;
   },
   X: function (a, b) {
     return a * b;
   },
-  "/": function (a, b) {
+  '/': function (a, b) {
     return a / b;
   },
 };
