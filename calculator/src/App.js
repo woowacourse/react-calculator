cd3371f148ec2d37f0bb6c6918cc50aef34c6f69
이아현 <lah1203@naver.com>
2022-04-19 19:39:15 +0900

    chore: 기본 설정
    Co-authored-by: Daye Lee <508yeah@gmail.com>

---
+++ calculator/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Calculator from './Calculator';
+
+class App extends React.Component {
+  render() {
+    return (
+      <main>
+        <Calculator />
+      </main>
+    );
+  }
+}
+
+export default App;
