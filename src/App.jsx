2501e4ff396665e6b02622fca818334f7e4c75a9
jswith <typingjs@gmail.com>
2022-04-19 20:36:26 +0900

    style: 기본 마크업 작성
    Co-authored-by: lokba <lokba@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,71 @@
 import './App.css';
+import React, { Component } from 'react';
 
-export default function App() {
-  return <div>hello</div>;
+class App extends Component {
+  // for error
+  componentDidMount() {}
+
+  render() {
+    return (
+      <div className="calculator">
+        <h1 id="total">0</h1>
+        <div className="digits flex">
+          <button type="button" className="digit">
+            9
+          </button>
+          <button type="button" className="digit">
+            8
+          </button>
+          <button type="button" className="digit">
+            7
+          </button>
+          <button type="button" className="digit">
+            6
+          </button>
+          <button type="button" className="digit">
+            5
+          </button>
+          <button type="button" className="digit">
+            4
+          </button>
+          <button type="button" className="digit">
+            3
+          </button>
+          <button type="button" className="digit">
+            2
+          </button>
+          <button type="button" className="digit">
+            1
+          </button>
+          <button type="button" className="digit">
+            0
+          </button>
+        </div>
+        <div className="modifiers subgrid">
+          <button type="button" className="modifier">
+            AC
+          </button>
+        </div>
+        <div className="operations subgrid">
+          <button type="button" className="operation">
+            /
+          </button>
+          <button type="button" className="operation">
+            X
+          </button>
+          <button type="button" className="operation">
+            -
+          </button>
+          <button type="button" className="operation">
+            +
+          </button>
+          <button type="button" className="operation">
+            =
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
+
+export default App;
