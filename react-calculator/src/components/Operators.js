9abfa337b19fcc2cfcd7cc8ce5bb2c62ca2a180d
cks3066 <cks3066@naver.com>
2022-04-19 20:59:58 +0900

    feat: 연산의 결과값이 Infinity일 경우 오류라는 문자열을 보여준다.
    Co-authored-by: Sunho Park <sunho6200@gmail.com>

--- react-calculator/src/components/Operators.js
+++ react-calculator/src/components/Operators.js
@@ -12,13 +12,7 @@ export default class Operators extends Component {
   }
 
   onClickOperator = (operator) => {
-    // 값이 입력되었을때에만 두번째 값을 받는다.
-    if (this.props.screenNumber === 0) {
-      return;
-    }
-
     if (operator !== '=') {
-      // 숫자 입력중 & 연산자 이미 있음
       if (this.props.숫자입력중 && this.state.firstNumber !== 0) {
         alert(
           "숫자는 2개까지만 입력할 수 있습니다. '='버튼을 눌러 계산을 해주세요."
@@ -31,14 +25,12 @@ export default class Operators extends Component {
       return;
     }
 
-    // '=' 클릭 시 초기화를 해준다.
     this.props.changeStep(false);
     this.setState({
       firstNumber: 0,
       operator: '',
     });
 
-    // 결과값을 스크린에 보여준다.
     switch (this.state.operator) {
       case '+':
         this.props.changeScreenNumber(
@@ -56,6 +48,10 @@ export default class Operators extends Component {
         );
         break;
       case '/':
+        if (this.props.screenNumber === 0) {
+          this.props.changeScreenNumber('오류');
+          return;
+        }
         this.props.changeScreenNumber(
           Math.floor(this.state.firstNumber / this.props.screenNumber)
         );
