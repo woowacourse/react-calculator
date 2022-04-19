2fa569af6676105fcfa57e0167bf4a25769a871a
Soyi Jeon <soyi.dev@gmail.com>
2022-04-19 17:54:08 +0900

    feat: 두 개의 숫자 사칙연산 구현
    Co-authored-by: DomMorello <zuzudnf@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,63 +1,161 @@
-function App() {
-  return (
-    <div className="App">
-      <div className="calculator">
-        <h1 id="total">0</h1>
-        <div className="digits flex">
-          <button className="digit" data-value="9">
-            9
-          </button>
-          <button className="digit" data-value="8">
-            8
-          </button>
-          <button className="digit" data-value="7">
-            7
-          </button>
-          <button className="digit" data-value="6">
-            6
-          </button>
-          <button className="digit" data-value="5">
-            5
-          </button>
-          <button className="digit" data-value="4">
-            4
-          </button>
-          <button className="digit" data-value="3">
-            3
-          </button>
-          <button className="digit" data-value="2">
-            2
-          </button>
-          <button className="digit" data-value="1">
-            1
-          </button>
-          <button className="digit" data-value="0">
-            0
-          </button>
-        </div>
-        <div className="modifiers subgrid">
-          <button className="modifier" id="clear-button">
-            AC
-          </button>
-        </div>
-        <div className="operations subgrid">
-          <button className="operation" data-operator="/">
-            /
-          </button>
-          <button className="operation" data-operator="x">
-            X
-          </button>
-          <button className="operation" data-operator="-">
-            -
-          </button>
-          <button className="operation" data-operator="+">
-            +
-          </button>
-          <button id="calculate-button">=</button>
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstNumber: '',
+      secondNumber: '',
+      operand: '',
+    };
+  }
+
+  isOverThreeDigit(number) {
+    return number.length >= 3;
+  }
+
+  setFirstNumber(number) {
+    if (this.isOverThreeDigit(this.state.firstNumber)) {
+      return;
+    }
+    this.setState((prevState) => ({
+      firstNumber: prevState.firstNumber + number,
+    }));
+  }
+
+  setSecondNumber(number) {
+    if (this.isOverThreeDigit(this.state.secondNumber)) {
+      return;
+    }
+    this.setState((prevState) => ({
+      secondNumber: prevState.secondNumber + number,
+    }));
+  }
+
+  handleNumber(e) {
+    if (this.state.operand === '') {
+      this.setFirstNumber(e.target.dataset.number);
+      return;
+    }
+    this.setSecondNumber(e.target.dataset.number);
+  }
+
+  handleModifier(e) {
+    console.log('AC');
+  }
+
+  calculate() {
+    switch (this.state.operand) {
+      case '+':
+        this.setState({
+          firstNumber: +this.state.firstNumber + +this.state.secondNumber,
+          secondNumber: '',
+          operand: '',
+        });
+        break;
+      case '-':
+        this.setState({
+          firstNumber: +this.state.firstNumber - +this.state.secondNumber,
+          secondNumber: '',
+          operand: '',
+        });
+        break;
+      case 'x':
+        this.setState({
+          firstNumber: +this.state.firstNumber * +this.state.secondNumber,
+          secondNumber: '',
+          operand: '',
+        });
+        break;
+      case '/':
+        this.setState({
+          firstNumber: +this.state.firstNumber / +this.state.secondNumber,
+          secondNumber: '',
+          operand: '',
+        });
+        break;
+      default:
+        break;
+    }
+  }
+
+  handleOperation(e) {
+    if (e.target.dataset.operator === '=') {
+      this.calculate();
+      return;
+    }
+
+    if (this.state.operand !== '') return;
+    this.setState({
+      operand: e.target.dataset.operator,
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="calculator">
+          <h1 id="total">
+            {this.state.firstNumber + this.state.operand + this.state.secondNumber}
+          </h1>
+          <div className="digits flex" onClick={this.handleNumber.bind(this)}>
+            <button className="digit" data-number="9">
+              9
+            </button>
+            <button className="digit" data-number="8">
+              8
+            </button>
+            <button className="digit" data-number="7">
+              7
+            </button>
+            <button className="digit" data-number="6">
+              6
+            </button>
+            <button className="digit" data-number="5">
+              5
+            </button>
+            <button className="digit" data-number="4">
+              4
+            </button>
+            <button className="digit" data-number="3">
+              3
+            </button>
+            <button className="digit" data-number="2">
+              2
+            </button>
+            <button className="digit" data-number="1">
+              1
+            </button>
+            <button className="digit" data-number="0">
+              0
+            </button>
+          </div>
+          <div className="modifiers subgrid" onClick={this.handleModifier.bind(this)}>
+            <button className="modifier" id="clear-button">
+              AC
+            </button>
+          </div>
+          <div className="operations subgrid" onClick={this.handleOperation.bind(this)}>
+            <button className="operation" data-operator="/">
+              /
+            </button>
+            <button className="operation" data-operator="x">
+              X
+            </button>
+            <button className="operation" data-operator="-">
+              -
+            </button>
+            <button className="operation" data-operator="+">
+              +
+            </button>
+            <button id="calculate-button" data-operator="=">
+              =
+            </button>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
