e7df491303d01d781d438e7cdab70c4314cdb449
YoungHwa Byun <rlaalsdl111@gmail.com>
2022-04-19 20:48:19 +0900

    feat: save current result to localstorage
    Co-authored-by: KangYunHo1221 <ansghkdbsgh@gmail.com>
    Co-authored-by: ronci <ronci@users.noreply.github.com>

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -5,42 +5,51 @@ class App extends Component {
   constructor() {
     super();
 
-    this.state = {
+    this.state = JSON.parse(localStorage.getItem('state')) ?? {
       total: 0,
       current: 0,
       operator: '',
+      isLastClickOperator: false,
     };
-    this.isLastClickOperator = false;
+  }
+
+  componentDidUpdate() {
+    localStorage.setItem('state', JSON.stringify(this.state));
   }
 
   handleDigitClick(buttonValue) {
-    if (this.isLastClickOperator === true) {
+    if (this.state.current.toString().length > 14) {
+      this.setState({ current: Infinity });
+      return;
+    }
+
+    if (this.state.isLastClickOperator === true) {
       this.setState({
         current: buttonValue,
+        isLastClickOperator: false,
       });
     }
 
-    if (this.isLastClickOperator === false) {
+    if (this.state.isLastClickOperator === false) {
       this.setState({
         current: this.state.current * 10 + buttonValue,
       });
     }
-
-    this.isLastClickOperator = false;
   }
 
   handleOperatorClick(operator) {
-    if (this.isLastClickOperator === true) {
+    if (this.state.isLastClickOperator === true) {
       this.setState({
         operator: operator,
       });
     }
 
-    if (this.isLastClickOperator === false) {
+    if (this.state.isLastClickOperator === false) {
       if (this.state.operator === '') {
         this.setState({
           total: this.state.current,
           operator: operator,
+          isLastClickOperator: true,
         });
       }
 
@@ -55,11 +64,10 @@ class App extends Component {
           total: result,
           current: result,
           operator: operator,
+          isLastClickOperator: true,
         });
       }
     }
-
-    this.isLastClickOperator = true;
   }
 
   operate(a, b, operator) {
@@ -82,6 +90,7 @@ class App extends Component {
       total: 0,
       current: 0,
       operator: '',
+      isLastClickOperator: false,
     });
   }
 
@@ -89,9 +98,17 @@ class App extends Component {
     return (
       <div className="App">
         <div className="calculator">
-          <h1 id="total">
-            {this.state.current === Infinity ? '오류' : this.state.current}
-          </h1>
+          <div className="total">
+            <h1
+              className={
+                this.state.current.toString().length >= 8
+                  ? ' small-total-font'
+                  : ''
+              }
+            >
+              {this.state.current === Infinity ? '오류' : this.state.current}
+            </h1>
+          </div>
           <div className="digits flex">
             {[9, 8, 7, 6, 5, 4, 3, 2, 1, 0].map(num => {
               return (
