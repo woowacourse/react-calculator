1482be5138bded2b1aa7ba9480ac46413f7c0556
sangminsong <ssmin940606@gmail.com>
2022-04-19 16:11:43 +0900

    feat: 마크업과 css 작성
    Co-authored-by: uk960214 <uk960214@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  const digitButtons = Array.from({ length: 10 }).map((val, index) => {
+    const buttonNumber = 9 - index;
+    return (
+      <button key={buttonNumber} className="digit">
+        {buttonNumber}
+      </button>
+    );
+  });
+  return (
+    <div className="App">
+      <div className="calculator">
+        <h1 id="total">0</h1>
+        <div className="digits flex">{digitButtons}</div>
+        <div className="modifiers subgrid">
+          <button className="modifier">AC</button>
+        </div>
+        <div className="operations subgrid">
+          <button className="operation">/</button>
+          <button className="operation">X</button>
+          <button className="operation">-</button>
+          <button className="operation">+</button>
+          <button className="operation">=</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
