288d35300019ba9d5f81470cab37aa5d6ea7a675
YongRae_Kim (Usage) <kyr9389@naver.com>
2022-04-19 15:19:24 +0900

    feat: 기본 마크업 작성

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import DisplayResult from "./component/DisplayResult";
+import CalculatorButton from "./component/CalculatorButton";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <>
+        <div id="app">
+          <div className="calculator">
+            <DisplayResult />
+            <CalculatorButton />
+          </div>
+        </div>
+      </>
+    );
+  }
 }
-
-export default App;
---
+++ src/component/DisplayResult.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class DisplayResult extends Component {
+  render() {
+    return <h1 id="total">0</h1>;
+  }
+}
