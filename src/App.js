15a29c3f299befa1a0b770cef846149b42bcbe04
YongRae_Kim (Usage) <kyr9389@naver.com>
2022-04-19 17:24:55 +0900

    feat: 연산 기능 구현

--- src/App.js
+++ src/App.js
@@ -3,13 +3,81 @@ import DisplayResult from "./component/DisplayResult";
 import CalculatorButton from "./component/CalculatorButton";
 
 export default class App extends Component {
+  state = {
+    firstNumber: null,
+    operator: null,
+    secondNumber: null,
+    result: 0,
+  };
+
+  onSetFirstNumber = (number) => {
+    this.setState({
+      firstNumber: Number(number),
+    });
+  };
+
+  onSetOperator = (operator) => {
+    this.setState({
+      operator: operator,
+    });
+  };
+
+  onSetSecondNumber = (number) => {
+    this.setState(
+      {
+        secondNumber: Number(number),
+      },
+      () => this.calculate()
+    );
+  };
+
+  calculate = () => {
+    const res = (() => {
+      switch (this.state.operator) {
+        case "+":
+          return this.add();
+        case "-":
+          return this.sub();
+        case "X":
+          return this.multiple();
+        case "/":
+          return this.divide();
+        default:
+          throw new Error("존재하지 않는 연산자입니다.");
+      }
+    })();
+
+    this.setState({ result: res }, () => console.log(this.state));
+  };
+
+  add() {
+    return this.state.firstNumber + this.state.secondNumber;
+  }
+
+  sub() {
+    return this.state.firstNumber - this.state.secondNumber;
+  }
+
+  divide() {
+    return Math.floor(this.state.firstNumber / this.state.secondNumber);
+  }
+
+  multiple() {
+    return this.state.firstNumber * this.state.secondNumber;
+  }
+
   render() {
     return (
       <>
         <div id="app">
           <div className="calculator">
             <DisplayResult />
-            <CalculatorButton />
+            <CalculatorButton
+              onSetFirstNumber={this.onSetFirstNumber}
+              onSetOperator={this.onSetOperator}
+              onSetSecondNumber={this.onSetSecondNumber}
+              calculate={this.calculate}
+            />
           </div>
         </div>
       </>
--- src/component/CalculatorButton.js
+++ src/component/CalculatorButton.js
@@ -1,10 +1,46 @@
 import React, { Component } from "react";
 
 export default class CalculatorButton extends Component {
+  state = {
+    firstNumber: "",
+    secondNumber: "",
+    isFirstNumber: true,
+  };
+
+  onClickNumber = (e) => {
+    if (this.state.isFirstNumber) {
+      this.setState({
+        firstNumber: this.state.firstNumber + e.target.textContent,
+      });
+      return;
+    }
+
+    this.setState({
+      secondNumber: this.state.secondNumber + e.target.textContent,
+    });
+  };
+
+  onClickOperator = (e) => {
+    if (e.target.textContent !== "=") {
+      this.props.onSetFirstNumber(this.state.firstNumber);
+      this.props.onSetOperator(e.target.textContent);
+      this.setState({
+        isFirstNumber: false,
+      });
+      return;
+    }
+
+    this.props.onSetSecondNumber(this.state.secondNumber);
+  };
+
+  onClickModifier = (e) => {
+    console.log(e.target.textContent);
+  };
+
   render() {
     return (
       <>
-        <div className="digits flex">
+        <div className="digits flex" onClick={this.onClickNumber}>
           <button className="digit">9</button>
           <button className="digit">8</button>
           <button className="digit">7</button>
@@ -16,10 +52,10 @@ export default class CalculatorButton extends Component {
           <button className="digit">1</button>
           <button className="digit">0</button>
         </div>
-        <div className="modifiers subgrid">
+        <div className="modifiers subgrid" onClick={this.onClickModifier}>
           <button className="modifier">AC</button>
         </div>
-        <div className="operations subgrid">
+        <div className="operations subgrid" onClick={this.onClickOperator}>
           <button className="operation">/</button>
           <button className="operation">X</button>
           <button className="operation">-</button>
