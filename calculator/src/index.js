efe1b4ff2a22ba27cd43fbb9cc8ebdec25265425
YoungHwa Byun <rlaalsdl111@gmail.com>
2022-04-20 18:00:19 +0900

    refactor: simplify code
    details
    
    - move function position
    - change class name
    - separate constants
    - simplify methods
    
    Co-authored-by: KangYunHo1221 <ansghkdbsgh@gmail.com>
    Co-authored-by: ronci <ronci@users.noreply.github.com>

--- calculator/src/index.js
+++ calculator/src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import Calculator from './Calculator';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Calculator />
   </React.StrictMode>
 );
+
+window.addEventListener('beforeunload', function (event) {
+  event.returnValue = '';
+});
