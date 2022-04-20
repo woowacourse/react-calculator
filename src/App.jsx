3b1643f736244127b94681f6bd78c5d89ba3c36a
최광희 <polyckh@gmail.com>
2022-04-20 20:57:58 +0900

    feat: 사칙연산 구현
    Co-authored-by: Jeongmin Woo <greenblues1190@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,115 @@ import React, { Component } from 'react';
 import './App.css';
 import Button from './Button';
 
+const DIGITS = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
+const OPERATORS = ['/', 'X', '-', '+'];
+const EQUAL = '=';
+
+const calculate = (expression) => {
+  const operator = Array.from(expression).find((char) =>
+    OPERATORS.includes(char)
+  );
+  const [firstNumber, secondNumber] = expression
+    .split(operator)
+    .map((numberString) => Number(numberString));
+
+  switch (operator) {
+    case '+':
+      return firstNumber + secondNumber;
+    case '-':
+      return firstNumber - secondNumber;
+    case 'X':
+      return firstNumber * secondNumber;
+    case '/':
+      return secondNumber === 0
+        ? Infinity
+        : parseInt(firstNumber / secondNumber, 10);
+    default:
+      throw new Error('error');
+  }
+};
+
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      expression: '',
+      result: '0',
+    };
+
+    this.handleDigitClick = this.handleDigitClick.bind(this);
+    this.handleOperationClick = this.handleOperationClick.bind(this);
+    this.handleEqualClick = this.handleEqualClick.bind(this);
+    this.handleACClick = this.handleACClick.bind(this);
+  }
+
+  handleDigitClick(digit) {
+    this.setState((prevState) => ({
+      expression: `${prevState.expression}${digit}`,
+      result: `${prevState.expression}${digit}`,
+    }));
+  }
+
+  handleOperationClick(operation) {
+    this.setState((prevState) => ({
+      expression: `${prevState.expression}${operation}`,
+      result: `${prevState.expression}${operation}`,
+    }));
+  }
+
+  handleEqualClick() {
+    const { expression } = this.state;
+    const result = calculate(expression);
+
+    this.setState({
+      expression: '',
+      result: result === Infinity ? '오류' : result.toString(),
+    });
+  }
+
+  handleACClick() {
+    this.setState({
+      expression: '',
+      result: '0',
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <div className="calculator">
-          <h1 id="total">0</h1>
+          <h1 id="total">{this.state.result}</h1>
           <div className="digits flex">
-            <Button className="digit keypad" text="9" />
-            <Button className="digit keypad" text="8" />
-            <Button className="digit keypad" text="7" />
-            <Button className="digit keypad" text="6" />
-            <Button className="digit keypad" text="5" />
-            <Button className="digit keypad" text="4" />
-            <Button className="digit keypad" text="3" />
-            <Button className="digit keypad" text="2" />
-            <Button className="digit keypad" text="1" />
-            <Button className="digit keypad" text="0" />
+            {DIGITS.map((digit) => (
+              <Button
+                key={digit}
+                className="digit keypad"
+                onClick={this.handleDigitClick}
+                text={digit.toString()}
+              />
+            ))}
           </div>
           <div className="modifiers subgrid">
-            <Button className="modifier keypad" text="AC" />
+            <Button
+              onClick={this.handleACClick}
+              className="modifier keypad"
+              text="AC"
+            />
           </div>
           <div className="operations subgrid">
-            <Button className="operation keypad" text="/" />
-            <Button className="operation keypad" text="X" />
-            <Button className="operation keypad" text="-" />
-            <Button className="operation keypad" text="+" />
-            <Button className="operation keypad" text="=" />
+            {OPERATORS.map((operator) => (
+              <Button
+                key={operator}
+                onClick={this.handleOperationClick}
+                className="operation keypad"
+                text={operator}
+              />
+            ))}
+            <Button
+              onClick={this.handleEqualClick}
+              className="operation keypad"
+              text={EQUAL}
+            />
           </div>
         </div>
       </div>
--- src/Button.jsx
+++ src/Button.jsx
@@ -4,7 +4,13 @@ import PropTypes from 'prop-types';
 class Button extends Component {
   render() {
     return (
-      <button className={this.props.className} type="button">
+      <button
+        className={this.props.className}
+        onClick={() => {
+          this.props.onClick(this.props.text);
+        }}
+        type="button"
+      >
         {this.props.text}
       </button>
     );
@@ -14,6 +20,7 @@ class Button extends Component {
 Button.propTypes = {
   className: PropTypes.string,
   text: PropTypes.string,
+  onClick: PropTypes.func,
 };
 
 export default Button;
