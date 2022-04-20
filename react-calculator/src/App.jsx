25424ea98d866fa13935683405723727917ff43f
Ahn <jin7969@gmail.com>
2022-04-20 21:26:48 +0900

    :sparkles: feat(ui): 계산기 숫자 랜더링
    Co-authored-by: rladpwl0512 <rladpwl0512@gmail.com>

--- react-calculator/src/App.jsx
+++ react-calculator/src/App.jsx
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Digits from './components/digits.jsx';
+import Operations from './components/operations.jsx';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.myRef = React.createRef();
+  }
+
   state = {
     operation: '',
     firstNumber: '',
@@ -11,38 +17,47 @@ class App extends Component {
 
   handleDigit = (number) => {
     if (this.state.operation) {
+      const secondNumberResult = this.state.secondNumber + number;
       this.setState({
         secondNumber:
-          this.state.secondNumber.length === 3
-            ? this.state.secondNumber
-            : this.state.secondNumber + number,
+          this.state.secondNumber.length === 3 ? this.state.secondNumber : secondNumberResult,
       });
+      this.renderCalculatorNumber(secondNumberResult);
       return;
     }
+
+    const firstNumberResult = this.state.firstNumber + number;
     this.setState({
-      firstNumber:
-        this.state.firstNumber.length === 3
-          ? this.state.firstNumber
-          : this.state.firstNumber + number,
+      firstNumber: this.state.firstNumber.length === 3 ? this.state.firstNumber : firstNumberResult,
     });
+    this.renderCalculatorNumber(firstNumberResult);
+  };
+
+  setOperations = (selectedOperation) => {
+    this.setState({ operation: selectedOperation });
+  };
+
+  add = () => {
+    const result = Number(this.state.firstNumber) + Number(this.state.secondNumber);
+    console.log('result', result);
+  };
+
+  renderCalculatorNumber = (number) => {
+    this.myRef.current.textContent = number;
   };
 
   render() {
     return (
       <div id="app">
         <div className="calculator">
-          <h1 id="total">0</h1>
+          <h1 id="calculator-number" ref={this.myRef}>
+            0
+          </h1>
           <Digits handleDigit={this.handleDigit} />
           <div className="modifiers subgrid">
             <button className="modifier">AC</button>
           </div>
-          <div className="operations subgrid">
-            <button className="operation">/</button>
-            <button className="operation">X</button>
-            <button className="operation">-</button>
-            <button className="operation">+</button>
-            <button className="operation">=</button>
-          </div>
+          <Operations add={this.add} setOperation={this.setOperations} />
         </div>
       </div>
     );
--- react-calculator/src/index.js
+++ react-calculator/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
+// eslint-disable-next-line import/extensions
 import ReactDOM from 'react-dom/client';
 import './index.css';
+// eslint-disable-next-line import/extensions
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
