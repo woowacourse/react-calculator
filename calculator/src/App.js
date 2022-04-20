ae32341067800bc0e3d5227f9352b2194aeb42b8
JUDONGHYEOK <judonghyeok@gmail.com>
2022-04-20 19:27:41 +0900

    style: 계산기 가운데 정렬
    Co-authored-by: jhy979 <jhy979@gmail.com>

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,8 +1,12 @@
 import { Component } from 'react';
 import Calculator from './component/Calculator';
-
+import './App.css';
 export default class App extends Component {
   render() {
-    return <Calculator></Calculator>;
+    return (
+      <div id="app">
+        <Calculator></Calculator>
+      </div>
+    );
   }
 }
