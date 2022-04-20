f24a983389c7a3af0bffb1b06e2842543c7cfbed
Hwang Jung Min <hwangstar156@gmail.com>
2022-04-20 12:02:10 +0900

    refactor: 매직넘버와 에러메시지들 상수화하여 관리

--- react-calculator/src/util/validator.js
+++ react-calculator/src/util/validator.js
@@ -1,17 +1,19 @@
+import { ERROR_MESSAGE, MAX_NUMBER_LENGTH } from "./constant";
+
 export const checkMaxNumberLength = (numbers, offset) => {
-  if (numbers[offset].length >= 3) {
-    throw new Error("숫자는 3자리수까지 입력가능합니다");
+  if (numbers[offset].length >= MAX_NUMBER_LENGTH) {
+    throw new Error(ERROR_MESSAGE.EXCEED_MAX_NUMBER_LENGTH);
   }
 };
 
 export const checkValidOperation = (numbers, offset) => {
   if (!numbers[0] || offset > 0) {
-    throw new Error("올바른 입력을 해주세요");
+    throw new Error(ERROR_MESSAGE.VALID_OPERATION);
   }
 };
 
 export const checkValidEqualOperation = (numbers) => {
   if (!numbers[1]) {
-    throw new Error("완전한 계산식을 입력하세요");
+    throw new Error(ERROR_MESSAGE.COMPLETE_FORMULAR);
   }
 };
