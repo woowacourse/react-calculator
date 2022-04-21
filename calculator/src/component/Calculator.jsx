ad78a624ba8171af9fb5221968808fe3d75a8def
JUDONGHYEOK <65863017+JUDONGHYEOK@users.noreply.github.com>
2022-04-22 01:07:42 +0900

    [1단계 - 계산기] 동키콩(주동혁) 미션 제출합니다. (#22)
    * feat: 계산 관련 도메인 추가
    
    * chore: eslint, prettier 설정 및 불필요한 주석제거
    
    Co-authored-by: jhy979 <jhy979@gmail.com>
    
    * feat: 계산기 View 생성
    
    Co-authored-by: jhy979 <jhy979@gmail.com>
    
    * feat: 계산 수행 기능 추가
    
    Co-authored-by: jhy979 <jhy979@gmail.com>
    
    * feat: AC 기능 추가
    
    Co-authored-by: jhy979 <jhy979@gmail.com>
    
    * feat: 연산의 결과가 Infinity일 경우 오류 문자 렌더링
    
    Co-authored-by: jhy979 <jhy979@gmail.com>
    
    * feat: localStorage에 결과값 저장 및 불러오기 기능 추가
    
    Co-authored-by: jhy979 <jhy979@gmail.com>
    
    * style: 계산기 가운데 정렬
    
    Co-authored-by: jhy979 <jhy979@gmail.com>
    
    * refactor: operator 버튼 map그룹핑
    
    Co-authored-by: jhy979 <jhy979@gmail.com>
    
    * refator: result 변수 추상화, 개행 추가
    
    Co-authored-by: jhy979 <jhy979@gmail.com>
    
    * feat: 마지막 출력값이 아닌 전체 계산 과정 저장
    
    Co-authored-by: jhy979 <jhy979@gmail.com>
    
    * refactor: calculator 상수화
    
    Co-authored-by: jhy979 <jhy979@gmail.com>
    
    * fix: nextNumber가 null일시 0이 자동바인딩 되는 문제 해결
    
    Co-authored-by: jhy979 <jhy979@gmail.com>
    
    * style: button hover & focus style 추가
    
    Co-authored-by: jhy979 <jhy979@gmail.com>
    
    * build: deploy 설정 추가
    
    Co-authored-by: jhy979 <jhy979@gmail.com>
    
    * fix: html lang 속성 변경
    
    Co-authored-by: jhy979 <jhy979@gmail.com>
    
    Co-authored-by: jhy979 <jhy979@gmail.com>

---
+++ calculator/src/component/Calculator.jsx
@@ -0,0 +1,120 @@
+import { Component } from 'react';
+import './Calculator.css';
+import { calculator } from '../domain/calculator';
+import { CALCULATOR, ERROR_MESSAGE, LOCAL_STORAGE_KEY } from '../constant';
+export default class Calculator extends Component {
+  constructor() {
+    super();
+
+    const {
+      prevNumber = 0,
+      nextNumber = null,
+      operator = '',
+    } = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) ?? {};
+
+    this.state = {
+      prevNumber: Number(prevNumber),
+      nextNumber: nextNumber && Number(nextNumber),
+      operator: operator,
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener('beforeunload', this.confirmExit);
+    window.addEventListener('unload', this.saveResult);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('beforeunload', this.confirmExit);
+    window.removeEventListener('unload', this.saveResult);
+  }
+
+  confirmExit = (e) => {
+    e.preventDefault();
+    e.returnValue = '';
+  };
+
+  saveResult = () => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(this.state));
+  };
+
+  changeNumber = (e) => {
+    if (!Number.isFinite(this.state.prevNumber)) {
+      this.initialize();
+    }
+
+    this.setState({
+      nextNumber: this.state.nextNumber * CALCULATOR.UNIT + Number(e.target.textContent),
+    });
+  };
+
+  calculate = (e) => {
+    if (!Number.isFinite(this.state.prevNumber)) return;
+
+    if (this.state.nextNumber === null || this.state.operator === '=') {
+      this.setState({ operator: e.target.textContent });
+      return;
+    }
+
+    if (this.state.operator === '') {
+      this.setState({
+        prevNumber: this.state.nextNumber,
+        nextNumber: null,
+        operator: e.target.textContent,
+      });
+      return;
+    }
+
+    this.setState({
+      prevNumber: calculator[this.state.operator](
+        this.state.prevNumber,
+        this.state.nextNumber
+      ),
+      nextNumber: null,
+      operator: e.target.textContent,
+    });
+  };
+
+  initialize = () => {
+    this.setState({
+      prevNumber: 0,
+      nextNumber: null,
+      operator: '',
+    });
+  };
+
+  render() {
+    const result =
+      this.state.nextNumber === null ? this.state.prevNumber : this.state.nextNumber;
+    return (
+      <div className="calculator">
+        <h1 id="total">
+          {Number.isFinite(this.state.prevNumber) ? result : ERROR_MESSAGE.INFINITY_ERROR}
+        </h1>
+        <div className="digits flex">
+          {new Array(CALCULATOR.MAX_NUMBER + 1).fill().map((_, idx) => (
+            <button
+              className="digit"
+              key={CALCULATOR.MAX_NUMBER - idx}
+              onClick={this.changeNumber}
+            >
+              {CALCULATOR.MAX_NUMBER - idx}
+            </button>
+          ))}
+        </div>
+        <div className="modifiers subgrid">
+          <button className="modifier" onClick={this.initialize}>
+            AC
+          </button>
+        </div>
+        <div className="operations subgrid">
+          {[...CALCULATOR.OPERATOR].map((operator, idx) => (
+            <button key={idx} className="operation" onClick={this.calculate}>
+              {operator}
+            </button>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
---
+++ calculator/src/domain/calculator.js
@@ -0,0 +1,14 @@
+export const calculator = {
+  '+': function (a, b) {
+    return a + b;
+  },
+  '-': function (a, b) {
+    return a - b;
+  },
+  X: function (a, b) {
+    return a * b;
+  },
+  '/': function (a, b) {
+    return a / b;
+  },
+};
