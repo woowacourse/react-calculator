708e14bd272e584b362d2d1c3d5c591b8ffcd4d5
YongRae_Kim (Usage) <kyr9389@naver.com>
2022-04-21 11:44:26 +0900

    refactor: 중복값 변수화
    Co-authored-by: Koy <inkyo.dev@gmail.com>

--- src/component/CalculatorButton.js
+++ src/component/CalculatorButton.js
@@ -2,30 +2,30 @@ import React, { Component } from "react";
 
 export default class CalculatorButton extends Component {
   onClickNumber = (e) => {
+    const inputNumber = e.target.textContent;
     if (this.props.result === 0) {
-      this.props.setResult(e.target.textContent);
+      this.props.setResult(inputNumber);
     } else {
-      this.props.setResult(this.props.result + e.target.textContent);
+      this.props.setResult(this.props.result + inputNumber);
     }
 
     if (this.props.isFirstNumber) {
       this.props.setFirstNumber(
-        this.props.firstNumber * 10 + Number(e.target.textContent)
+        this.props.firstNumber * 10 + Number(inputNumber)
       );
       return;
     }
-
-    this.props.setSecondNumber(this.props.secondNumber + e.target.textContent);
+    this.props.setSecondNumber(this.props.secondNumber + inputNumber);
   };
 
   onClickOperator = (e) => {
+    const inputOperator = e.target.textContent;
     if (this.props.firstNumber === "") return;
-    if (e.target.textContent === "=" && this.props.secondNumber === "") return;
-    this.props.setResult(this.props.result + e.target.textContent);
-
-    if (e.target.textContent !== "=") {
+    if (inputOperator === "=" && this.props.secondNumber === "") return;
+    this.props.setResult(this.props.result + inputOperator);
+    if (inputOperator !== "=") {
       this.props.setFirstNumber(this.props.firstNumber);
-      this.props.setOperator(e.target.textContent);
+      this.props.setOperator(inputOperator);
       this.props.setIsFirstNumber(false);
       return;
     }
