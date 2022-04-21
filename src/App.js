1b0f073ea5551a1940cffff873955069db85a56b
Koy <inkyo.dev@gmail.com>
2022-04-21 23:27:48 +0900

    [1단계 - 계산기] 코이(정인교) 미션 제출합니다. (#16)
    * chore: create react app init
    
    * feat: 기본 마크업 작성
    
    * feat: 연산 기능 구현
    
    * feat: 초기화(AC) 기능 구현
    
    Co-authored-by: Koy <inkyo.dev@gmail.com>
    
    * refactor: 나누어져있던 state를 한 곳으로 일원화
    
    Co-authored-by: Koy <inkyo.dev@gmail.com>
    
    * fix: 연속 계산이 안되는 문제 수정
    
    Co-authored-by: Koy <inkyo.dev@gmail.com>
    
    * feat: 이전 값 localStorage에 저장 후 불러오기 구현
    
    Co-authored-by: Koy <inkyo.dev@gmail.com>
    
    * feat: 페이지 이동시 confirm 표시
    
    Co-authored-by: Koy <inkyo.dev@gmail.com>
    
    * feat: Infinity, NaN을 오류로 표기하는 기능 구현
    
    Co-authored-by: Koy <inkyo.dev@gmail.com>
    
    * docs: 요구 사항 문서 수정
    
    Co-authored-by: Koy <inkyo.dev@gmail.com>
    
    * chore: gh-pages 설치
    
    Co-authored-by: Koy <inkyo.dev@gmail.com>
    
    * refactor: 중복값 변수화
    Co-authored-by: usageness <kyr9389@naver.com>
    
    Co-authored-by: YongRae_Kim (Usage) <kyr9389@naver.com>

---
+++ src/App.js
@@ -0,0 +1,137 @@
+import React, { Component } from "react";
+import DisplayResult from "./component/DisplayResult";
+import CalculatorButton from "./component/CalculatorButton";
+
+export default class App extends Component {
+  state = {
+    firstNumber: 0,
+    operator: null,
+    secondNumber: 0,
+    isFirstNumber: true,
+    result: 0,
+  };
+
+  componentDidMount() {
+    const prevValue = localStorage.getItem("prevValue") || 0;
+    this.setFirstNumber(Number(prevValue));
+    this.setResult(Number(prevValue));
+
+    window.addEventListener("beforeunload", function (e) {
+      e.preventDefault();
+      e.returnValue = "";
+    });
+  }
+
+  initState = () => {
+    this.setState({
+      firstNumber: 0,
+      operator: null,
+      secondNumber: 0,
+      isFirstNumber: true,
+      result: 0,
+    });
+  };
+
+  setFirstNumber = (number) => {
+    this.setState(
+      {
+        firstNumber: Number(number),
+      },
+      localStorage.setItem("prevValue", number)
+    );
+  };
+
+  setOperator = (operator) => {
+    this.setState({ operator });
+  };
+
+  setSecondNumber = (number) => {
+    this.setState(
+      {
+        secondNumber: Number(number),
+      },
+      localStorage.setItem("prevValue", number)
+    );
+  };
+
+  setResult = (result) => {
+    this.setState({ result });
+  };
+
+  setIsFirstNumber = (isFirstNumber) => {
+    this.setState({ isFirstNumber });
+  };
+
+  calculate = () => {
+    const res = (() => {
+      switch (this.state.operator) {
+        case "+":
+          return this.add();
+        case "-":
+          return this.sub();
+        case "X":
+          return this.multiple();
+        case "/":
+          return this.divide();
+        default:
+          throw new Error("존재하지 않는 연산자입니다.");
+      }
+    })();
+
+    this.setState({ result: res }, () => {
+      this.initState();
+
+      if (res === Infinity || isNaN(res)) {
+        this.setFirstNumber(0);
+        this.setResult("오류");
+        localStorage.setItem("prevValue", "오류");
+        return;
+      }
+
+      this.setFirstNumber(res);
+      this.setResult(res);
+      localStorage.setItem("prevValue", res);
+    });
+  };
+
+  add() {
+    return this.state.firstNumber + this.state.secondNumber;
+  }
+
+  sub() {
+    return this.state.firstNumber - this.state.secondNumber;
+  }
+
+  divide() {
+    return Math.floor(this.state.firstNumber / this.state.secondNumber);
+  }
+
+  multiple() {
+    return this.state.firstNumber * this.state.secondNumber;
+  }
+
+  render() {
+    return (
+      <>
+        <div id="app">
+          <div className="calculator">
+            <DisplayResult result={this.state.result} />
+            <CalculatorButton
+              setFirstNumber={this.setFirstNumber}
+              setOperator={this.setOperator}
+              setSecondNumber={this.setSecondNumber}
+              setIsFirstNumber={this.setIsFirstNumber}
+              calculate={this.calculate}
+              setResult={this.setResult}
+              result={this.state.result}
+              initState={this.initState}
+              isFirstNumber={this.state.isFirstNumber}
+              firstNumber={this.state.firstNumber}
+              secondNumber={this.state.secondNumber}
+            />
+          </div>
+        </div>
+      </>
+    );
+  }
+}
