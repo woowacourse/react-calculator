e3d763ab54ce4d6b3f89f4f884c138efb00fe473
Ahn <jin7969@gmail.com>
2022-04-22 22:56:04 +0900

    :recycle: refactor: 계산기 숫자버튼 마크업 로직 변경

--- react-calculator/src/components/digits.jsx
+++ react-calculator/src/components/digits.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { DIGIT_LIST } from '../constants.js';
 
 class Digits extends Component {
   handleDigitButtonClick = (e) => {
@@ -7,37 +8,12 @@ class Digits extends Component {
 
   render() {
     return (
-      <div className="digits flex">
-        <button className="digit" onClick={this.handleDigitButtonClick}>
-          9
-        </button>
-        <button className="digit" onClick={this.handleDigitButtonClick}>
-          8
-        </button>
-        <button className="digit" onClick={this.handleDigitButtonClick}>
-          7
-        </button>
-        <button className="digit" onClick={this.handleDigitButtonClick}>
-          6
-        </button>
-        <button className="digit" onClick={this.handleDigitButtonClick}>
-          5
-        </button>
-        <button className="digit" onClick={this.handleDigitButtonClick}>
-          4
-        </button>
-        <button className="digit" onClick={this.handleDigitButtonClick}>
-          3
-        </button>
-        <button className="digit" onClick={this.handleDigitButtonClick}>
-          2
-        </button>
-        <button className="digit" onClick={this.handleDigitButtonClick}>
-          1
-        </button>
-        <button className="digit" onClick={this.handleDigitButtonClick}>
-          0
-        </button>
+      <div className="digits flex" onClick={this.handleDigitButtonClick}>
+        {DIGIT_LIST.map((digit, index) => (
+          <button className="digit" key={index}>
+            {digit}
+          </button>
+        ))}
       </div>
     );
   }
