66b449f79a099a80c239e9438ec12b59c1351d36
moonheekim0118 <hellomooneekim@gmail.com>
2022-04-22 15:12:57 +0900

    fix: 최대 3자리수만 입력 가능하도록 수정

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -20,7 +20,6 @@ const hasInput = ({ firstOperand, secondOperand, operation }) => {
   return firstOperand !== '0' || secondOperand !== '' || operation !== null;
 };
 
-/** 생명 주기 -> 각자 어떤 부분을 학습해볼까? 애기해보기 */
 class Calculator extends Component {
   constructor() {
     super();
@@ -59,13 +58,19 @@ class Calculator extends Component {
 
     if (this.state.operation) {
       this.setState(({ secondOperand }) => ({
-        secondOperand: `${Number(secondOperand + digit)}`,
+        secondOperand:
+          secondOperand.length >= 3
+            ? `${secondOperand.slice(0, -1) + digit}`
+            : `${Number(secondOperand + digit)}`,
         isError: false,
       }));
       return;
     }
     this.setState(({ firstOperand }) => ({
-      firstOperand: `${Number(firstOperand + digit)}`,
+      firstOperand:
+        firstOperand.length >= 3
+          ? `${firstOperand.slice(0, -1) + digit}`
+          : `${Number(firstOperand + digit)}`,
       isError: false,
     }));
   };
@@ -86,7 +91,7 @@ class Calculator extends Component {
     if (isFinite(result)) {
       this.setState((prevState) => ({
         ...prevState,
-        firstOperand: result,
+        firstOperand: `${result}`,
         secondOperand: '',
         operation: null,
       }));
@@ -109,37 +114,40 @@ class Calculator extends Component {
 
   render() {
     return (
-      <div className="calculator">
-        <h1 id="total">
-          {this.state.isError
-            ? '오류'
-            : `${this.state.firstOperand}
+      <>
+        <div>세 자리 수 까지만 입력 가능합니다</div>
+        <div className="calculator">
+          <h1 id="total">
+            {this.state.isError
+              ? '오류'
+              : `${this.state.firstOperand}
             ${this.state.operation ?? ''}
             ${this.state.secondOperand}`}
-        </h1>
-        <div className="digits flex" onClick={this.onClickDigits}>
-          <button className="digit">9</button>
-          <button className="digit">8</button>
-          <button className="digit">7</button>
-          <button className="digit">6</button>
-          <button className="digit">5</button>
-          <button className="digit">4</button>
-          <button className="digit">3</button>
-          <button className="digit">2</button>
-          <button className="digit">1</button>
-          <button className="digit">0</button>
+          </h1>
+          <div className="digits flex" onClick={this.onClickDigits}>
+            <button className="digit">9</button>
+            <button className="digit">8</button>
+            <button className="digit">7</button>
+            <button className="digit">6</button>
+            <button className="digit">5</button>
+            <button className="digit">4</button>
+            <button className="digit">3</button>
+            <button className="digit">2</button>
+            <button className="digit">1</button>
+            <button className="digit">0</button>
+          </div>
+          <div className="modifiers subgrid" onClick={this.initState}>
+            <button className="modifier">AC</button>
+          </div>
+          <div className="operations subgrid" onClick={this.onClickOperations}>
+            <button className="operation">/</button>
+            <button className="operation">X</button>
+            <button className="operation">-</button>
+            <button className="operation">+</button>
+            <button className="operation">=</button>
+          </div>
         </div>
-        <div className="modifiers subgrid" onClick={this.initState}>
-          <button className="modifier">AC</button>
-        </div>
-        <div className="operations subgrid" onClick={this.onClickOperations}>
-          <button className="operation">/</button>
-          <button className="operation">X</button>
-          <button className="operation">-</button>
-          <button className="operation">+</button>
-          <button className="operation">=</button>
-        </div>
-      </div>
+      </>
     );
   }
 }
