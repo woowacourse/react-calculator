b13d15a6fd9678a97d75ad98eae7891a906730ff
gytks4 <gytks4@naver.com>
2022-04-22 16:08:09 +0900

    refactor: 리렌더링 필요없는 변수를 컴포넌트 바깥으로 분리

--- src/components/CalculatorInputField/Digits.jsx
+++ src/components/CalculatorInputField/Digits.jsx
@@ -4,15 +4,17 @@ class Digits extends React.Component {
   render() {
     return (
       <div className="digits flex">
-        {[9, 8, 7, 6, 5, 4, 3, 2, 1, 0].map((digit) => (
-          <button
-            className="digit"
-            key={digit}
-            onClick={this.props.handleClickDigit}
-          >
-            {digit}
-          </button>
-        ))}
+        {Array(10)
+          .fill()
+          .map((_, digit) => (
+            <button
+              className="digit"
+              key={digit}
+              onClick={this.props.handleClickDigit}
+            >
+              {9 - digit}
+            </button>
+          ))}
       </div>
     );
   }
--- src/components/CalculatorInputField/Operators.jsx
+++ src/components/CalculatorInputField/Operators.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
+const OPERATORS = ['/', 'X', '-', '+', '='];
+
 class Operators extends React.Component {
   render() {
     return (
       <div className="operations subgrid">
-        {['/', 'X', '-', '+', '='].map((operator) => (
+        {OPERATORS.map((operator) => (
           <button
             className="operation"
             key={operator}
