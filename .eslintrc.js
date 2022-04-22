e483f786e81e86d1d4697d19f612114308764ded
moonheekim0118 <moonhee118118@icloud.com>
2022-04-22 22:55:31 +0900

    [1단계 - 계산기] 호프(김문희) 미션 제출합니다. (#5)
    * docs: 요구사항 정리
    
    * chore: CRA 프로젝트 셋팅
    
    * chore: lint, prettier 설정
    
    * chore: lint 재설정
    
    * feat: Calculator 컴포넌트의 마크업, 스타일링 개발
    
    * feat: 계산 기능 구현
    
    * feat: 페이지 이탈 기능 구현
    
    * docs: 기능 구현 완료
    
    * refactor: 상태 초기화 로직을 constructor로 이동
    
    * refactor: 상태를 기록하는 로직을 최적화
    
    * refactor: 초기화 하는 로직을 함수로 분리
    
    * refactor: 입력된 값이 있는지를 판단하는 조건식 함수로 분리
    
    * refactor: 오류 값을 검증하는 로직을 수정
    
    * chore: netlify deploy 설정 파일 추가
    
    * fix: 최대 3자리수만 입력 가능하도록 수정
    
    * fix: digits 클릭 시, 전체 숫자가 출력되는 문제 해결
    
    * fix: 나눔시 소수점 아래는 버리도록 수정
    
    * refactor: App.js 제거
    
    * refactor: 이벤트 위임 제거
    
    * refactor: 불필요한 spread 문법 제거
    
    * refactor: 불필요한 return 제거
    
    Co-authored-by: junzzi <jjh01027717502@gmail.com>

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+/* lint 설정 이야기를 좀 더 해보자 */
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/react-in-jsx-scope': 0,
+  },
+};
---
+++ src/components/Calculator.js
@@ -0,0 +1,177 @@
+import { Component } from 'react';
+import '../styles/Calculator.css';
+
+const computeExpression = ({ firstOperand, secondOperand, operation }) => {
+  if (operation === '/') {
+    return Math.floor(firstOperand / secondOperand);
+  }
+  if (operation === 'X') {
+    return firstOperand * secondOperand;
+  }
+  if (operation === '-') {
+    return firstOperand - secondOperand;
+  }
+  if (operation === '+') {
+    return firstOperand + secondOperand;
+  }
+};
+
+const hasInput = ({ firstOperand, secondOperand, operation }) =>
+  firstOperand !== '0' || secondOperand !== '' || operation !== null;
+
+const computeNextOperand = (currentOperand, digit) => {
+  return currentOperand.length >= 3
+    ? `${currentOperand.slice(0, -1)}${digit}`
+    : `${Number(currentOperand + digit)}`;
+};
+
+class Calculator extends Component {
+  constructor() {
+    super();
+    const memoizedState = JSON.parse(localStorage.getItem('prevState'));
+    this.state = memoizedState
+      ? memoizedState
+      : {
+          firstOperand: '0',
+          secondOperand: '',
+          operation: null,
+          isError: false,
+        };
+  }
+
+  componentDidMount() {
+    window.addEventListener('beforeunload', this.onBeforeUnload);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('beforeunload', this.onBeforeUnload);
+    localStorage.setItem('prevState', JSON.stringify(this.state));
+  }
+
+  initState = () => {
+    this.setState({
+      firstOperand: '0',
+      secondOperand: '',
+      operation: null,
+      isError: false,
+    });
+  };
+
+  onClickDigit = ({ target }) => {
+    const { textContent: digit } = target;
+    this.state.operation
+      ? this.setState(({ secondOperand }) => ({
+          secondOperand: computeNextOperand(secondOperand, digit),
+          isError: false,
+        }))
+      : this.setState(({ firstOperand }) => ({
+          firstOperand: computeNextOperand(firstOperand, digit),
+          isError: false,
+        }));
+  };
+
+  onClickOperation = ({ target }) => {
+    const { textContent: operation } = target;
+    if (operation !== '=') {
+      this.setState({ operation });
+      return;
+    }
+
+    const result = computeExpression({
+      firstOperand: Number(this.state.firstOperand),
+      secondOperand: Number(this.state.secondOperand),
+      operation: this.state.operation,
+    });
+
+    if (isFinite(result)) {
+      this.setState({
+        firstOperand: `${result}`,
+        secondOperand: '',
+        operation: null,
+      });
+      return;
+    }
+
+    this.setState(() => ({
+      isError: true,
+    }));
+  };
+
+  onBeforeUnload = (e) => {
+    e.preventDefault();
+    localStorage.setItem('prevState', JSON.stringify(this.state));
+    if (hasInput(this.state)) {
+      e.returnValue = '';
+    }
+  };
+
+  render() {
+    return (
+      <>
+        <div>숫자는 3자리 까지만 입력이 가능합니다.</div>
+        <div className="calculator">
+          <h1 id="total">
+            {this.state.isError
+              ? '오류'
+              : `${this.state.firstOperand}
+            ${this.state.operation ?? ''}
+            ${this.state.secondOperand}`}
+          </h1>
+          <div className="digits flex">
+            <button className="digit" onClick={this.onClickDigit}>
+              9
+            </button>
+            <button className="digit" onClick={this.onClickDigit}>
+              8
+            </button>
+            <button className="digit" onClick={this.onClickDigit}>
+              7
+            </button>
+            <button className="digit" onClick={this.onClickDigit}>
+              6
+            </button>
+            <button className="digit" onClick={this.onClickDigit}>
+              5
+            </button>
+            <button className="digit" onClick={this.onClickDigit}>
+              4
+            </button>
+            <button className="digit" onClick={this.onClickDigit}>
+              3
+            </button>
+            <button className="digit" onClick={this.onClickDigit}>
+              2
+            </button>
+            <button className="digit" onClick={this.onClickDigit}>
+              1
+            </button>
+            <button className="digit" onClick={this.onClickDigit}>
+              0
+            </button>
+          </div>
+          <div className="modifiers subgrid" onClick={this.initState}>
+            <button className="modifier">AC</button>
+          </div>
+          <div className="operations subgrid">
+            <button className="operation" onClick={this.onClickOperation}>
+              /
+            </button>
+            <button className="operation" onClick={this.onClickOperation}>
+              X
+            </button>
+            <button className="operation" onClick={this.onClickOperation}>
+              -
+            </button>
+            <button className="operation" onClick={this.onClickOperation}>
+              +
+            </button>
+            <button className="operation" onClick={this.onClickOperation}>
+              =
+            </button>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+export default Calculator;
