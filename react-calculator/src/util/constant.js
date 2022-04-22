7fee963ea7e49a3e27fdf11546fd924ae368567b
soobin <38878617+sanaandmomo@users.noreply.github.com>
2022-04-22 18:46:29 +0900

    [1단계 - 계산기] 아놀드(한수빈) 미션 제출합니다. (#36)
    * chore: create-react-app 설치
    
    * docs: 기능 구현 사항 문서 추가
    
    * feat: 숫자 클릭시 화면에 보이도록 기능 추가
    
    * feat: 3자리수이상 입력시 예외처리 코드 추가
    
    * feat: 연산자 입력시 화면에 출력이 되도록 추가
    
    * feat: = 입력시 예외처리 추가
    
    * feat: AC버튼 기능 추가
    
    * feat: 계산된 상태에서의 상황 처리 추가
    
    * feat: 0으로 나누었을때 오류라고 보여줄수있도록 설정
    
    * feat: 페이지를 나갔을때 마지막 결과값을 저장하는 기능 추가
    
    * refactor: 예외처리하는 함수 util로 빼서 관리
    
    * refactor: 매직넘버와 에러메시지들 상수화하여 관리
    
    * docs: 기능 구현 완료
    
    * chore: gh-pages 설치
    
    Co-authored-by: Hwang Jung Min <hwangstar156@gmail.com>

---
+++ react-calculator/src/util/constant.js
@@ -0,0 +1,7 @@
+export const MAX_NUMBER_LENGTH = 3;
+
+export const ERROR_MESSAGE = {
+  EXCEED_MAX_NUMBER_LENGTH: `숫자는 ${MAX_NUMBER_LENGTH}자리수까지 입력가능합니다`,
+  VALID_OPERATION: "올바른 입력을 해주세요",
+  COMPLETE_FORMULAR: "완전한 계산식을 입력하세요",
+};
