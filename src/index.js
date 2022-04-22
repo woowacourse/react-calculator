20aaeef87de405823f5966aeba795f8464d50487
junzzi <jjh01027717502@gmail.com>
2022-04-22 16:49:11 +0900

    refactor: App.js, App.css 제거

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
-import App from './App';
+import Calculator from './components/Calculator';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Calculator></Calculator>
   </React.StrictMode>,
 );
