ee45c97a5b16b3117e50da012daea59615cf9c13
airman5573 <68623798+airman5573@users.noreply.github.com>
2022-04-23 13:42:03 +0900

    [1단계 - 계산기] 병민(윤병인) 미션 제출합니다. (#34)
    * chore(settings): commit message를 정형화 한다
    
    * chore(settings): eslint와 prettier를 설정한다
    
    * chore(settings): webpack과 typescript를 설정한다
    
    * chore(settings): tracking 하지 않을 파일들을 지정한다
    
    * feat: 계산기를 생성한다
    
    * refactor(cleanup): 불필요한 코드를 삭제한다
    
    * fix: eSLint를 적용한다

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Calculator from './Calculator';
+import './scss/style.scss';
+
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+root.render(
+  <Calculator />
+)
