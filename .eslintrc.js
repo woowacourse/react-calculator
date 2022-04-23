791b62eed6d8bbb1a76f1b17d5d1bebcaea02238
이아현 <lah1203@naver.com>
2022-04-23 17:56:26 +0900

    chore: babel parser 및 rule 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,13 +5,24 @@ module.exports = {
     node: true,
   },
   extends: ['airbnb-base', 'plugin:prettier/recommended'],
-  plugins: 'prettier',
+  plugins: ['prettier'],
+  parser: '@babel/eslint-parser',
   parserOptions: {
     ecmaVersion: 2021,
     sourceType: 'module',
+    requireConfigFile: false,
+    babelOptions: {
+      presets: ['@babel/preset-react'],
+    },
   },
   rules: {
+    'import/no-unresolved': 'off',
+    'import/extensions': 'off',
+    'consistent-return': 'off',
+    'default-case': 'off',
+    'no-alert': 'off',
+    'no-unused-vars': 'off',
+    'no-use-before-define': 'off',
     'max-depth': [1, 2],
-    'max-lines-per-function': [1, 15],
   },
 };
