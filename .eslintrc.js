ea5ccc19825a98b76be9106e834bcb9ba1dd2125
Marco <59413128+wonsss@users.noreply.github.com>
2022-04-23 09:37:28 +0900

    [1단계 - 계산기] 마르코(장원석) 미션 제출합니다. (#26)
    * docs : 테스트
    
    * chore : cra, eslint, prettier 환경 설정
    
    * feat: 계산기 화면 렌더
    
    * feat: 버튼 눌렀을 때 해당 값을 렌더링
    
    * feat: 연산 결과 렌더링
    
    * feat: Infifinty 결과값 도출 시 오류 렌더링
    
    * feat: 계산 결과를 표현할 때 소수점 이하는 버림
    
    * feat: 숫자는 한번에 최대 3자리 수까지 입력 가능하다
    
    * fixed: 음수에 대한 연산 에러 해결
    
    * feat: 로컬스토리지에 상태저장 및 불러오기 구현
    
    * feat: 페이지 이탈시 conifrm한다
    
    * chore: 불필요한 파일 및 주석 정리
    
    * fixed: 연산 결과 null 발생 에러 해결
    
    * style: 가운데 정렬
    
    * chore: html 수정
    
    * chore: 환경설정

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/destructuring-assignment': [0],
+    'react/prop-types': [0],
+    'react/prefer-stateless-function': [0],
+  },
+};
---
+++ src/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import Calculator from './Calculator';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Calculator />
+  </React.StrictMode>,
+);
+
+reportWebVitals();
