93e8f8f10f61478ba6e2ad638b41da918d32657b
이아현 <lah1203@naver.com>
2022-04-23 19:45:37 +0900

    refactor: 사용하지 않는 상수 제거

--- src/constants.js
+++ src/constants.js
@@ -1,11 +1,9 @@
 const RULE = {
   MAX_DIGIT_LENGTH: 3,
-  MAX_OPERATION_COUNT: 1,
 };
 
 const ERROR_MESSAGE = {
   IS_OVER_MAX_DIGIT_LENGTH: `${RULE.MAX_DIGIT_LENGTH}자리까지 입력할 수 있어요.`,
-  IS_OVER_MAX_OPERATION_COUNT: `연산자는 ${RULE.MAX_OPERATION_COUNT}개까지 입력할 수 있어요.`,
   INFINITY_TOTAL: '오류',
 };
 
