5b490721194d4b77f4c7a521320dc74cabbed91b
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-04-23 14:31:37 +0900

    [1단계 - 계산기] 콤피(류현승) 미션 제출합니다. (#18)
    * chore: 기본 패키지 설치
    
    * feat: 연산자 상태값 업데이트 추가
    
    * chore: 프리티어 패키지 설정 변경
    
    * feat: 숫자 상태값 업데이트 추가
    
    * feat: 계산 결과 상태값 업데이트 추가
    
    * feat: 결과값 출력 기능 구현
    
    * feat: 초기화 기능 구현
    
    * feat: 페이지 이탈 시 상태값 저장 및 컨펌 처리 기능 구현
    
    * refactor: JSX 리팩터링
    
    * refactor: 계산기 리팩터링
    
    * docs: 문서 업데이트
    
    * chore: gh-pages 설치
    
    * chore: gh-pages 홈페이지 설정
    
    * style: 타이틀 변경
    
    Co-authored-by: woose28 <dldntmdsla@naver.com>

---
+++ src/Calculator.js
@@ -0,0 +1,139 @@
+import React, { Component } from 'react';
+import OperationButton from './OperationButton';
+import './Calculator.css';
+
+class Calculator extends Component {
+  MAX_NUMBER = 1000;
+  DIGIT_LIST = Array.from({ length: 10 }, (_, index) => 9 - index);
+  OPERATION_LIST = ['/', 'X', '-', '+'];
+
+  constructor(props) {
+    super(props);
+
+    this.state = this.initState();
+  }
+
+  get defaultState() {
+    return {
+      numbers: [0, 0],
+      operator: '',
+    };
+  }
+
+  initState() {
+    const savedState = localStorage.getItem('CALCULATOR_STATE');
+    if (savedState) {
+      return JSON.parse(savedState);
+    }
+
+    return this.defaultState;
+  }
+
+  #isExceedMaxNumber(number) {
+    return number >= this.MAX_NUMBER && number !== Infinity;
+  }
+
+  componentDidMount() {
+    window.addEventListener('beforeunload', this.onBeforeUnload);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('beforeunload', this.onBeforeUnload);
+  }
+
+  onBeforeUnload = (event) => {
+    event.preventDefault();
+    event.returnValue = '';
+
+    localStorage.setItem('CALCULATOR_STATE', JSON.stringify(this.state));
+  };
+
+  onClickAllClear = () => {
+    this.setState(this.defaultState);
+  };
+
+  onClickDigit = ({ target }) => {
+    const numberIndex = this.state.operator === '' ? 0 : 1;
+    const newStateNumbers = [...this.state.numbers];
+    newStateNumbers[numberIndex] = newStateNumbers[numberIndex] * 10 + Number(target.innerText);
+
+    if (newStateNumbers[numberIndex] === Infinity) {
+      alert('무한한 숫자는 입력할 수 없어, 입력값을 초기화합니다.');
+      this.resetState();
+      return;
+    }
+
+    if (this.#isExceedMaxNumber(newStateNumbers[numberIndex])) {
+      alert('숫자는 세 자리까지 입력 가능합니다.');
+      return;
+    }
+
+    this.setState({ numbers: newStateNumbers });
+  };
+
+  onClickResult = () => {
+    const { operator, numbers } = this.state;
+
+    if (operator === '') {
+      alert('연산자를 입력해주세요.');
+      return;
+    }
+
+    const operatorCollection = {
+      '+': () => numbers[0] + numbers[1],
+      '-': () => numbers[0] - numbers[1],
+      X: () => numbers[0] * numbers[1],
+      '/': () => numbers[0] / numbers[1],
+    };
+
+    const resultNumber = Number(operatorCollection[operator]().toFixed(2));
+    this.setState({ numbers: [resultNumber, 0], operator: '' });
+  };
+
+  setOperator = (operator) => {
+    this.setState({ operator });
+  };
+
+  render() {
+    const { operator, numbers } = this.state;
+    let totalNumber = !operator || numbers[1] === 0 ? numbers[0] : numbers[1];
+    if (totalNumber === Infinity) totalNumber = '오류';
+
+    const operationButtonProps = {
+      currentOperator: operator,
+      setOperator: this.setOperator,
+    };
+
+    return (
+      <div className="App">
+        <div className="calculator">
+          <h1 id="total">{totalNumber}</h1>
+          <div className="digits flex">
+            {this.DIGIT_LIST.map((digit) => (
+              <button key={digit} className="digit" onClick={this.onClickDigit}>
+                {digit}
+              </button>
+            ))}
+          </div>
+          <div className="modifiers subgrid">
+            <button className="modifier" onClick={this.onClickAllClear}>
+              AC
+            </button>
+          </div>
+          <div className="operations subgrid">
+            {this.OPERATION_LIST.map((operation) => (
+              <OperationButton key={operation} {...operationButtonProps}>
+                {operation}
+              </OperationButton>
+            ))}
+            <button className="operation" onClick={this.onClickResult}>
+              =
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Calculator;
