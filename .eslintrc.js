ee9c4d3f11af9c853e79f8226e41c7807669aaee
woose28 <dldntmdsla@naver.com>
2022-04-24 21:07:08 +0900

    chore: eslint 설정 파일 생성

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  extends: 'react-app',
+};
