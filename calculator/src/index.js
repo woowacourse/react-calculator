d88407744efaf9bc3e94f1bf7f9c41896d82ef65
2019015414 <2019015414@hanyang.ac.kr>
2022-04-24 21:55:13 +0900

    feat: change to functional components
    change class components to functional components

--- calculator/src/index.js
+++ calculator/src/index.js
@@ -1,14 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Calculator from './Calculator';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Calculator from "./Calculator";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Calculator />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Calculator />);
 
-window.addEventListener('beforeunload', function (event) {
-  event.returnValue = '';
+window.addEventListener("beforeunload", function (event) {
+  event.returnValue = "";
 });
