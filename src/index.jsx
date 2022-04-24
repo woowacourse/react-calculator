ed4838ee16229e83a0a1b746fb11a1727dd58ecb
jihyeok-um <75592315+jihyeok-um@users.noreply.github.com>
2022-04-24 23:05:57 +0900

    chore: 파일 정리

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
 /* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
-=======
-import React from 'react';
->>>>>>> 682c5cb5153c056c511a116314077abc9db0ad3a
+
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
@@ -12,11 +9,7 @@ import Calculator from './Calculator';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-<<<<<<< HEAD
     <Calculator />
-=======
-    <Calculator />,
->>>>>>> 682c5cb5153c056c511a116314077abc9db0ad3a
   </React.StrictMode>,
 );
 
