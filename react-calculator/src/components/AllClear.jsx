30c4ba72784c028eb22ef856cc2fb309c7022eb2
KIM YEJI <rladpwl0512@gmail.com>
2022-04-25 00:45:39 +0900

    :recycle: refactor: 필요없는 함수 삭제

--- react-calculator/src/components/AllClear.jsx
+++ react-calculator/src/components/AllClear.jsx
@@ -1,11 +1,7 @@
 function AllClear({ allClear }) {
-  const handleAllClearButtonClick = () => {
-    allClear();
-  };
-
   return (
     <div className="allClear subgrid">
-      <button className="allClearButton" onClick={handleAllClearButtonClick}>
+      <button className="allClearButton" onClick={allClear}>
         AC
       </button>
     </div>
