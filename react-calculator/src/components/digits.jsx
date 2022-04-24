55b11d5305bb20fd8d1e49584e492c2dcfce2032
KIM YEJI <rladpwl0512@gmail.com>
2022-04-24 18:26:16 +0900

    :recycle: refactor: 계산기 숫자를 배열로 렌더링 하도록 수정

--- react-calculator/src/components/digits.jsx
+++ react-calculator/src/components/digits.jsx
@@ -1,42 +1,18 @@
 import React from 'react';
 
 function Digits(props) {
+  const digits = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
   const handleDigitButtonClick = (e) => {
     props.handleDigit(e.target.textContent);
   };
 
   return (
     <div className="digits flex">
-      <button className="digit" onClick={handleDigitButtonClick}>
-        9
-      </button>
-      <button className="digit" onClick={handleDigitButtonClick}>
-        8
-      </button>
-      <button className="digit" onClick={handleDigitButtonClick}>
-        7
-      </button>
-      <button className="digit" onClick={handleDigitButtonClick}>
-        6
-      </button>
-      <button className="digit" onClick={handleDigitButtonClick}>
-        5
-      </button>
-      <button className="digit" onClick={handleDigitButtonClick}>
-        4
-      </button>
-      <button className="digit" onClick={handleDigitButtonClick}>
-        3
-      </button>
-      <button className="digit" onClick={handleDigitButtonClick}>
-        2
-      </button>
-      <button className="digit" onClick={handleDigitButtonClick}>
-        1
-      </button>
-      <button className="digit" onClick={handleDigitButtonClick}>
-        0
-      </button>
+      {digits.map((digit) => (
+        <button className="digit" onClick={handleDigitButtonClick} key={digit}>
+          {digit}
+        </button>
+      ))}
     </div>
   );
 }
