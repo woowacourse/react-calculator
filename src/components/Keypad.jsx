0f227e5feb42c2d06f93c9c062e65debe7d3efae
최광희 <polyckh@gmail.com>
2022-04-25 14:21:47 +0900

    refactor: 함수형 컴포넌트로 마이그레이션

--- src/components/Keypad.jsx
+++ src/components/Keypad.jsx
@@ -1,26 +1,21 @@
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import React from 'react';
 import Button from './Button';
 
-export default class Keypad extends Component {
-  render() {
-    const { className, keyClassName, keypad, onClick } = this.props;
-
-    return (
-      <div className={className}>
-        {keypad.map((key) => (
-          <Button
-            key={key}
-            className={keyClassName}
-            onClick={onClick}
-            text={key.toString()}
-          />
-        ))}
-      </div>
-    );
-  }
+export default function Keypad({ className, keyClassName, keypad, onClick }) {
+  return (
+    <div className={className}>
+      {keypad.map((key) => (
+        <Button
+          key={key}
+          className={keyClassName}
+          onClick={onClick}
+          text={key.toString()}
+        />
+      ))}
+    </div>
+  );
 }
-
 Keypad.propTypes = {
   className: PropTypes.string,
   keyClassName: PropTypes.string,
